f0b0471d146d6edc07c035890960fe0eece7206e
NeetuK2018 <neetu.k.atwal@googlemail.com>
2019-02-17 14:32:02 +0000

    fixed readme

--- home.js
+++ home.js
@@ -1,14 +1,39 @@
 const endPoints = {
-  '/api/topics': 'Lists all Topics',
-  '/api/topics/:topic/articles': 'Lists Articles for Selected Topic',
-  '/api/articles': 'Lists All Articles',
-  '/api/articles/:article_id': 'Displays Article by Article ID',
-  '/api/articles/:article_id/comments': 'Lists all the Comments by Article ID',
-  '/api/articles/:article_id/comments/:comment_id':
-    'Displays Comment by selected ID for selected Article ID',
-  '/api/users': 'Lists all Users',
-  '/api/users/:username': 'Displays User details for selected Username',
-  '/api/users/:username/articles': 'Lists all the Articles by selected Username',
+  '/api/topics': {
+    GET: 'shows all topics available',
+    POST: 'allows a new topic to be added {slug, description}',
+  },
+  '/api/topics/:topic/articles': {
+    GET:
+      'shows the articles associated with the selected topic, also takes queries on limit, sort_by, order, and p(page)',
+    POST: 'allows a new article to be added {title, body, author} and topic_id',
+  },
+  '/api/articles': {
+    GET: 'shows all articles, also takes queries on limit, sort_by, order, and p(page)',
+  },
+  '/api/articles/:article_id': {
+    GET: 'shows articles by selected id',
+    PATCH: 'allows user to vote {inc_votes} with an integer',
+    DELETE: 'deletes article with article_id and responds with no content',
+  },
+  '/api/articles/:article_id/comments': {
+    GET:
+      'shows all the comments associated with selected article, also takes queries on limit, sort_by, order, and p(page)',
+    POST: 'takes a {username, body} and adds to the specifid article_id',
+  },
+  '/api/articles/:article_id/comments/:comment_id': {
+    PATCH: 'accepts votes {inc_votes} when an integet is passed and increments',
+    DELETE: 'deletes a comment specified by the comment_id and responds with no content',
+  },
+  '/api/users': {
+    GET: 'displays all users',
+    POST: 'allows a new user to be added {username, avatar_url, name}',
+  },
+  '/api/users/:username': { GET: 'shows a specific user by chosen username' },
+  '/api/users/:username/articles': {
+    GET:
+      'shows all the articles associated with a user, also takes queries on limit, sort_by, order, and p(page)',
+  },
 };
 
 module.exports = { endPoints };
